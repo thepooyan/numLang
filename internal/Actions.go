59c2767cd8dc8264cd80405779fb27928e24f2af
pooyan <thepooyan@gmail.com>
2025-05-06 18:39:22 +0330

    nothing!

--- internal/Actions.go
+++ internal/Actions.go
@@ -73,13 +73,13 @@ func (program *Program) input(args ...string) {
 }
 
 func (program *Program) variable(args ...string) {
-  if len(args) != 5 || args[3] != "=" {
+  if len(args) < 5 || args[3] != "=" {
     println("Invalid syntax:")
     command := strings.Join(args, " ")
     println(command)
     os.Exit(1)
   }
-  err := program.addVariable(args[1], args[2], args[4])
+  err := program.addVariable(args[1], args[2], strings.Join(args[4:], " ") )
   if err != nil {
     println(err.Error())
     os.Exit(1)
